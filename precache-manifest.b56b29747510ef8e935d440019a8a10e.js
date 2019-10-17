6f0c46a7aa4a0a6c525096b6525ab7f266232370
nikolaounkl <nikolaounkl@gmail.com>
2019-10-17 17:13:42 +0200

    Updates

---
+++ precache-manifest.b56b29747510ef8e935d440019a8a10e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "376e002f06730efe5078482f6d610db8",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "c5f59d456a99544f94dd",
+    "url": "/me/static/js/2.ecf49e07.chunk.js"
+  },
+  {
+    "revision": "f16c0afe1e4fc0a68a9c",
+    "url": "/me/static/js/main.6881264b.chunk.js"
+  },
+  {
+    "revision": "0228127eee9e9ea8b75c",
+    "url": "/me/static/js/runtime-main.cebf21f2.js"
+  }
+]);
